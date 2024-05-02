1383a0450ba541955201c6b95c8f0d0f39e7578e
True-Mousse <101472540+True-Mousse@users.noreply.github.com>
2024-05-01 17:50:12 -0700

    Create SenseHat.py

---
+++ SenseHat.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import os
+import datetime
+from sense_hat import SenseHat
+
+file_name = datetime.date.today()
+file_path = "/etc/docker/" + str(file_name) + ".txt"
+
+sense = SenseHat()
+sense.clear()
+temp_C = round(sense.get_temperature())
+# temp_F = (9*temp_C)/5+32
+
+hour = datetime.datetime.now().hour
+minute = datetime.datetime.now().minute
+
+time = str(hour) + "h" + str(minute) + "m: "
+current_Temp = str(temp_C) + "C\n"
+
+if os.path.exists(file_path):
+	with open (file_path, 'a') as fp:
+ 		fp.write(time + current_Temp)
+		
+else:
+ 	with open(file_path, 'w') as fp:
+ 		fp.write(time + current_Temp)
